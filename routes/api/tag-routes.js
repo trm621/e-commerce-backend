30f4d7595b8f500acdedf5044883abb74077d260
trm621 <t.maher621@gmail.com>
2022-01-10 16:51:11 -0500

    began building tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,6 +4,24 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  Tag.findAll({
+    include: [
+      {
+        model: ProductTag,
+        attributes: ['product_id'],
+        include: 
+        {
+          model: Product,
+          attributes: ['product_name']
+        }  
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // find all tags
   // be sure to include its associated Product data
 });
