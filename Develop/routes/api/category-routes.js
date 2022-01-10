594989d192f024b6bc78bc3b2c1f12901b5b5e70
trm621 <t.maher621@gmail.com>
2022-01-09 22:36:28 -0500

    created category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,24 +4,86 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err);
+      res.status(500).json(err);
+  
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ]
+  })
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+   Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbProductData => {
+    res.status(200).json(dbProductData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    individualHooks: true,
+    where: { id: req.params.id}
+  })
+  .then(dbProductData => {
+    if (!dbProductData[0]) {
+      res.status(404)({message: 'No category with this id.'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: { id: req.params.id}
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({message: 'No category with this id.'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // delete a category by its `id` value
 });
 
